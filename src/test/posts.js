3985eb5df9f89aa6f16d76b325bc995f9592e0ac
Joseph Cottingham <joseph.h.cottingham@gmail.com>
2021-02-15 13:48:58 -0500

    Fixed posts test

--- src/test/posts.js
+++ src/test/posts.js
@@ -6,53 +6,89 @@ const expect = chai.expect;
 
 // Import the Post model from our models folder so we
 // we can use it in our tests.
+const User = require('./../models/user');
 const Post = require('./../models/post');
 const server = require('../../server');
 
+
 chai.should();
 chai.use(chaiHttp);
+const agent = chai.request.agent(app);
 
-describe('Posts', function() {
-  const agent = chai.request.agent(server);
-  // Post that we'll use for testing purposes
-  const newPost = {
-      title: 'post title',
-      url: 'https://www.google.com',
-      summary: 'post summary'
-  };
-  it('Should create with valid attributes at POST /posts/new', function(done) {
-    // Checks how many posts there are now
-    Post.estimatedDocumentCount()
-      .then(function (initialDocCount) {
-          agent
-              .post("/posts/new")
-              // This line fakes a form post,
-              // since we're not actually filling out a form
-              .set("content-type", "application/x-www-form-urlencoded")
-              // Make a request to create another
-              .send(newPost)
+describe('Posts', function () {
+    const agent = chai.request.agent(server);
+    // Post that we'll use for testing purposes
+    const newPost = {
+        title: 'post title',
+        subreddit: 'post',
+        url: 'https://www.google.com',
+        summary: 'post summary'
+    };
+    const user = {
+        username: 'poststest',
+        password: 'testposts'
+    };
+    it('Should create with valid attributes at POST /posts/new', function (done) {
+        // Checks how many posts there are now
+        Post.estimatedDocumentCount()
+            .then(function (initialDocCount) {
+                agent
+                    .post("/posts/new")
+                    // This line fakes a form post,
+                    // since we're not actually filling out a form
+                    .set("content-type", "application/x-www-form-urlencoded")
+                    // Make a request to create another
+                    .send(newPost)
+                    .then(function (res) {
+                        Post.estimatedDocumentCount()
+                            .then(function (newDocCount) {
+                                // Check that the database has one more post in it
+                                expect(res).to.have.status(200);
+                                // Check that the database has one more post in it
+                                expect(newDocCount).to.be.equal(initialDocCount + 1)
+                                done();
+                            })
+                            .catch(function (err) {
+                                done(err);
+                            });
+                    })
+                    .catch(function (err) {
+                        done(err);
+                    });
+            })
+            .catch(function (err) {
+                done(err);
+            });
+    });
+    after(function (done) {
+        Post.findOneAndDelete(newPost)
+        .then(function (res) {
+            agent.close()
+      
+            User.findOneAndDelete({
+                username: user.username
+            })
               .then(function (res) {
-                  Post.estimatedDocumentCount()
-                      .then(function (newDocCount) {
-                          // Check that the database has one more post in it
-                          expect(res).to.have.status(200);
-                          // Check that the database has one more post in it
-                          expect(newDocCount).to.be.equal(initialDocCount + 1)
-                          done();
-                      })
-                      .catch(function (err) {
-                          done(err);
-                      });
+                  done()
               })
               .catch(function (err) {
                   done(err);
               });
-      })
-      .catch(function (err) {
-          done(err);
+        })
+        .catch(function (err) {
+            done(err);
+        });
       });
-  });
-  after(function () {
-    Post.findOneAndDelete(newPost);
-  });
+    before(function (done) {
+        agent
+          .post('/sign-up')
+          .set("content-type", "application/x-www-form-urlencoded")
+          .send(user)
+          .then(function (res) {
+            done();
+          })
+          .catch(function (err) {
+            done(err);
+          });
+      });
 });
